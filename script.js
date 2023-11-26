c409a4343db42244cc64b5da71dec66e916f9037
Namrata Dhobale <121574685+dhobalenamrata@users.noreply.github.com>
2023-11-26 22:44:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -77,5 +77,23 @@ window.onload = function() {
 /////////////////// Text animation section end //////////////////////////////////////////////
 /////////////////// onscroll navigation section started /////////////////////////////////////
 
+let sec = document.querySelectorAll('section');
+        let links = document.querySelectorAll('nav a');
+
+        window.onscroll = () => {
+            sec.forEach( section => {
+                let top = window.scrollY;
+                let offset = section.offsetTop;
+                let height = section.offsetHeight;
+                let id = section.getAttribute('id');
+
+                if (top >= offset && top < offset + height){
+                    links.forEach( link => {
+                       link.classList.remove('active'); 
+                       document.querySelector('nav a[href*=' + id + ']').classList.add('active');   
+                })
+              }
+            })
+        };
 
 ///////////////////// onscroll navigation section end ///////////////////////////////////////

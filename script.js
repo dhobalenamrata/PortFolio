7c9238587d23b2cf571b12b8a5137fa0f7c05934
Namrata Dhobale <121574685+dhobalenamrata@users.noreply.github.com>
2023-11-28 17:56:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// When the user scrolls down 20px from the top of the document, show the button
-/*
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    //mybutton.style.display = "block";
-    document.getElementById("nav").style.background = "rgba(255, 255, 255,0.8)";
-    document.getElementById("myBtn").style.display="block";
-  } else {
-    document.getElementById("nav").style.background = "transparent";
-   document.getElementById("myBtn").style.display="none";
-  }
-}
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}  */
-
 ///////////////// Text animation section started //////////////////////////////////////////////
 
 var TxtType = function(el, toRotate, period) {
@@ -98,4 +78,26 @@ let sec = document.querySelectorAll('section');
             })
         };
 
-///////////////////// onscroll navigation section end ///////////////////////////////////////
+///////////////////// onscroll navigation section end ////////////////////////////////
+//////////////////// onscrolltop function started here ///////////////////////////////
+
+// When the user scrolls down 20px from the top of the document, show the button
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    //mybutton.style.display = "block";
+    document.getElementById("nav").style.background = "rgba(255, 255, 255,0.8)";
+    document.getElementById("myBtn").style.display="block";
+  } else {
+    document.getElementById("nav").style.background = "transparent";
+   document.getElementById("myBtn").style.display="none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}  
+/////////////////////////// onscrolltop function end here /////////////////////////////////////
